95de2b6601b81487ac7d6aa9836dd0e9bdfd34bf
piomin <piotr.minkowski@gmail.com>
2021-10-05 23:07:14 +0200

    update #12

--- order-service/src/main/java/pl/piomin/samples/quarkus/serverless/order/service/OrderConfirmFunction.java
+++ order-service/src/main/java/pl/piomin/samples/quarkus/serverless/order/service/OrderConfirmFunction.java
@@ -1,12 +1,11 @@
 package pl.piomin.samples.quarkus.serverless.order.service;
 
-import io.quarkus.funqy.Funq;
 import org.jboss.logging.Logger;
 import pl.piomin.samples.quarkus.serverless.order.model.Order;
+import pl.piomin.samples.quarkus.serverless.order.model.OrderStatus;
 import pl.piomin.samples.quarkus.serverless.order.repository.OrderRepository;
 
 import javax.inject.Inject;
-import javax.transaction.Transactional;
 
 public class OrderConfirmFunction {
 
@@ -15,14 +14,32 @@ public class OrderConfirmFunction {
     @Inject
     OrderRepository repository;
 
-    @Funq
-    @Transactional
     public void confirm(Order order) {
         log.infof("Response order: %s", order);
-        // TODO - finish implementation
+
     }
 
     private void doConfirm(Order o) {
-        // TODO - finish implementation
+        Order order = repository.findById(o.getId());
+        log.infof("Order found: %s", order);
+        if (order.getStatus() == OrderStatus.NEW) {
+            order.setStatus(o.getStatus());
+            if (o.getStatus() == OrderStatus.REJECTED)
+                order.setRejectedService(o.getSource());
+        } else if (order.getStatus() == OrderStatus.IN_PROGRESS) {
+            if (o.getStatus() == OrderStatus.REJECTED)
+                order.setStatus(OrderStatus.ROLLBACK);
+            else
+                order.setStatus(OrderStatus.CONFIRMED);
+            log.infof("Order confirmed: %s", order);
+            sender.send(order);
+        } else if (order.getStatus() == OrderStatus.REJECTED) {
+            order.setStatus(OrderStatus.ROLLBACK);
+            order.setRejectedService(o.getSource());
+            log.infof("Order rejected: %s", order);
+            sender.send(order);
+        }
+        repository.persist(order);
+        log.infof("Order updated: %s", order);
     }
 }
