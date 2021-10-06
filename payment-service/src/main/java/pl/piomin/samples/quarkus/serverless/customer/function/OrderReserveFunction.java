242a67ddcee2da36a6a84223b276498641a5cc63
piomin <piotr.minkowski@gmail.com>
2021-10-07 00:38:11 +0200

    update #23

--- payment-service/src/main/java/pl/piomin/samples/quarkus/serverless/customer/function/OrderReserveFunction.java
+++ payment-service/src/main/java/pl/piomin/samples/quarkus/serverless/customer/function/OrderReserveFunction.java
@@ -1,7 +1,9 @@
 package pl.piomin.samples.quarkus.serverless.customer.function;
 
 import org.jboss.logging.Logger;
+import pl.piomin.samples.quarkus.serverless.customer.exception.NotFoundException;
 import pl.piomin.samples.quarkus.serverless.customer.message.Order;
+import pl.piomin.samples.quarkus.serverless.customer.model.Customer;
 import pl.piomin.samples.quarkus.serverless.customer.repository.CustomerRepository;
 
 import javax.inject.Inject;
@@ -21,11 +23,17 @@ public class OrderReserveFunction {
     }
 
     private void doReserve(Order order) {
-
+        Customer c = repository.findById(order.getCustomerId());
+        if (c == null)
+            throw new NotFoundException();
+        log.infof("Customer found: %s", c);
     }
 
     private void doConfirm(Order order) {
-
+        Customer c = repository.findById(order.getCustomerId());
+        if (c == null)
+            throw new NotFoundException();
+        log.infof("Customer found: %s", c);
     }
 
 }
