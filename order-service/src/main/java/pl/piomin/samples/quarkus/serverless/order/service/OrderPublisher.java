848a443182f260f3f254d5da05990d2695511d4b
piomin <piotr.minkowski@gmail.com>
2021-10-15 15:31:04 +0200

    fix #2

--- order-service/src/main/java/pl/piomin/samples/quarkus/serverless/order/service/OrderPublisher.java
+++ order-service/src/main/java/pl/piomin/samples/quarkus/serverless/order/service/OrderPublisher.java
@@ -2,6 +2,7 @@ package pl.piomin.samples.quarkus.serverless.order.service;
 
 import io.smallrye.mutiny.Multi;
 import io.smallrye.reactive.messaging.annotations.Broadcast;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.jboss.logging.Logger;
 import pl.piomin.samples.quarkus.serverless.order.model.Order;
@@ -24,13 +25,15 @@ public class OrderPublisher {
     private OrderRepository repository;
     @Inject
     private UserTransaction transaction;
+    @ConfigProperty(name = "app.orders.timeout")
+    private int timeout;
 
     @Outgoing("order-events")
     @Broadcast
     public Multi<Order> orderEventsPublish() {
-        return Multi.createFrom().ticks().every(Duration.ofSeconds(1))
+        return Multi.createFrom().ticks().every(Duration.ofMillis(timeout))
                 .map(tick -> {
-                    Order o = new Order(++num, (int) num%10+1, (int) num%10+1, 100, 1, OrderStatus.NEW);
+                    Order o = new Order(++num, (int) num%1000+1, (int) num%100+1, 100, 1, OrderStatus.NEW);
                     try {
                         transaction.begin();
                         repository.persist(o);
