29d4a6c8e56e1d257d348a1c09f8db8c9012de66
piomin <piotr.minkowski@gmail.com>
2021-10-15 17:40:40 +0200

    fix #7

--- order-service/src/main/java/pl/piomin/samples/quarkus/serverless/order/service/OrderConfirmFunction.java
+++ order-service/src/main/java/pl/piomin/samples/quarkus/serverless/order/service/OrderConfirmFunction.java
@@ -2,52 +2,22 @@ package pl.piomin.samples.quarkus.serverless.order.service;
 
 import io.quarkus.funqy.Funq;
 import org.jboss.logging.Logger;
-import pl.piomin.samples.quarkus.serverless.order.client.OrderSender;
 import pl.piomin.samples.quarkus.serverless.order.model.Order;
-import pl.piomin.samples.quarkus.serverless.order.model.OrderStatus;
-import pl.piomin.samples.quarkus.serverless.order.repository.OrderRepository;
 
 import javax.inject.Inject;
-import javax.persistence.LockModeType;
-import javax.transaction.Transactional;
 
 public class OrderConfirmFunction {
 
     @Inject
     Logger log;
     @Inject
-    OrderRepository repository;
-    @Inject
-    OrderSender publisher;
+    OrderService orderService;
 
     @Funq
-    @Transactional
     public void confirm(Order order) {
-        log.infof("Confirmed order: %s", order);
-        doConfirm(order);
+        log.infof("Accepted order: %s", order);
+        orderService.doConfirm(order);
     }
 
-    private void doConfirm(Order o) {
-        Order order = repository.findById(o.getId(), LockModeType.PESSIMISTIC_WRITE);
-        if (order == null) {
-            return;
-        } else if (order.getStatus() == OrderStatus.NEW) {
-            order.setStatus(o.getStatus());
-            if (o.getStatus() == OrderStatus.REJECTED)
-                order.setRejectedService(o.getSource());
-        } else if (order.getStatus() == OrderStatus.IN_PROGRESS) {
-            if (o.getStatus() == OrderStatus.REJECTED)
-                order.setStatus(OrderStatus.ROLLBACK);
-            else
-                order.setStatus(OrderStatus.CONFIRMED);
-            log.infof("Order confirmed: %s", order);
-            publisher.send(order);
-        } else if (order.getStatus() == OrderStatus.REJECTED) {
-            order.setStatus(OrderStatus.ROLLBACK);
-            order.setRejectedService(o.getSource());
-            log.infof("Order rejected: %s", order);
-            publisher.send(order);
-        }
-        repository.persist(order);
-    }
+
 }
---
+++ order-service/src/main/java/pl/piomin/samples/quarkus/serverless/order/service/OrderService.java
@@ -0,0 +1,48 @@
+package pl.piomin.samples.quarkus.serverless.order.service;
+
+import org.jboss.logging.Logger;
+import pl.piomin.samples.quarkus.serverless.order.client.OrderSender;
+import pl.piomin.samples.quarkus.serverless.order.model.Order;
+import pl.piomin.samples.quarkus.serverless.order.model.OrderStatus;
+import pl.piomin.samples.quarkus.serverless.order.repository.OrderRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.LockModeType;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+public class OrderService {
+
+    @Inject
+    Logger log;
+    @Inject
+    OrderRepository repository;
+    @Inject
+    OrderSender publisher;
+
+    @Transactional
+    public void doConfirm(Order o) {
+        Order order = repository.findById(o.getId(), LockModeType.PESSIMISTIC_WRITE);
+        if (order == null) {
+            return;
+        } else if (order.getStatus() == OrderStatus.NEW) {
+            order.setStatus(o.getStatus());
+            if (o.getStatus() == OrderStatus.REJECTED)
+                order.setRejectedService(o.getSource());
+        } else if (order.getStatus() == OrderStatus.IN_PROGRESS) {
+            if (o.getStatus() == OrderStatus.REJECTED)
+                order.setStatus(OrderStatus.ROLLBACK);
+            else
+                order.setStatus(OrderStatus.CONFIRMED);
+            log.infof("Order confirmed: %s", order);
+            publisher.send(order);
+        } else if (order.getStatus() == OrderStatus.REJECTED) {
+            order.setStatus(OrderStatus.ROLLBACK);
+            order.setRejectedService(o.getSource());
+            log.infof("Order rejected: %s", order);
+            publisher.send(order);
+        }
+        repository.persist(order);
+    }
+}
