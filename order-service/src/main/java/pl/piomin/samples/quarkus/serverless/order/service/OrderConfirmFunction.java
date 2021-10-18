cf90d200e51fcf43c42eefe48eaa22b00d535fe5
piomin <piotr.minkowski@gmail.com>
2021-10-18 23:20:29 +0200

    fix #3

--- order-service/src/main/java/pl/piomin/samples/quarkus/serverless/order/service/OrderConfirmFunction.java
+++ order-service/src/main/java/pl/piomin/samples/quarkus/serverless/order/service/OrderConfirmFunction.java
@@ -3,7 +3,6 @@ package pl.piomin.samples.quarkus.serverless.order.service;
 import org.jboss.logging.Logger;
 import pl.piomin.samples.quarkus.serverless.order.client.OrderSender;
 import pl.piomin.samples.quarkus.serverless.order.model.Order;
-import pl.piomin.samples.quarkus.serverless.order.model.OrderStatus;
 import pl.piomin.samples.quarkus.serverless.order.repository.OrderRepository;
 
 import javax.inject.Inject;
@@ -15,6 +14,8 @@ public class OrderConfirmFunction {
     @Inject
     OrderRepository repository;
     @Inject
+    OrderService orderService;
+    @Inject
     OrderSender sender;
 
     public void confirm(Order order) {
@@ -22,27 +23,4 @@ public class OrderConfirmFunction {
 
     }
 
-    private void doConfirm(Order o) {
-        Order order = repository.findById(o.getId());
-        log.infof("Order found: %s", order);
-        if (order.getStatus() == OrderStatus.NEW) {
-            order.setStatus(o.getStatus());
-            if (o.getStatus() == OrderStatus.REJECTED)
-                order.setRejectedService(o.getSource());
-        } else if (order.getStatus() == OrderStatus.IN_PROGRESS) {
-            if (o.getStatus() == OrderStatus.REJECTED)
-                order.setStatus(OrderStatus.ROLLBACK);
-            else
-                order.setStatus(OrderStatus.CONFIRMED);
-            log.infof("Order confirmed: %s", order);
-            sender.send(order);
-        } else if (order.getStatus() == OrderStatus.REJECTED) {
-            order.setStatus(OrderStatus.ROLLBACK);
-            order.setRejectedService(o.getSource());
-            log.infof("Order rejected: %s", order);
-            sender.send(order);
-        }
-        repository.persist(order);
-        log.infof("Order updated: %s", order);
-    }
 }
