4ad9bc011c33c652e38576dce080dfbe75da8d9e
piomin <piotr.minkowski@gmail.com>
2023-04-09 00:07:45 +0200

    update (2)

--- order-service/src/main/java/pl/piomin/samples/quarkus/serverless/order/OrderApplication.java
+++ order-service/src/main/java/pl/piomin/samples/quarkus/serverless/order/OrderApplication.java
@@ -1,13 +1,17 @@
 package pl.piomin.samples.quarkus.serverless.order;
 
 import io.quarkus.runtime.StartupEvent;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.context.ManagedExecutor;
 import org.jboss.logging.Logger;
 import pl.piomin.samples.quarkus.serverless.order.model.Order;
 import pl.piomin.samples.quarkus.serverless.order.model.OrderStatus;
 import pl.piomin.samples.quarkus.serverless.order.repository.OrderRepository;
+import pl.piomin.samples.quarkus.serverless.order.service.OrderPublisher;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 
 @ApplicationScoped
@@ -15,10 +19,12 @@ public class OrderApplication {
 
     Logger log;
     OrderRepository repository;
+    OrderPublisher publisher;
 
-    public OrderApplication(Logger log, OrderRepository repository) {
+    public OrderApplication(Logger log, OrderRepository repository, OrderPublisher publisher) {
         this.log = log;
         this.repository = repository;
+        this.publisher = publisher;
     }
 
     @Transactional
@@ -27,5 +33,23 @@ public class OrderApplication {
             Order o = new Order(null, i%1000+1, i%100+1, 100, 1, OrderStatus.NEW);
             repository.persist(o);
         }
+    }
+
+    @Inject
+    ManagedExecutor executor;
+    @ConfigProperty(name = "app.orders.timeout", defaultValue = "10000")
+    long timeout;
+
+    void execute(@Observes StartupEvent ev) {
+        executor.runAsync(() -> {
+            while (true) {
+               publisher.generate();
+                try {
+                    Thread.sleep(timeout);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 }
--- order-service/src/main/java/pl/piomin/samples/quarkus/serverless/order/service/OrderPublisher.java
+++ order-service/src/main/java/pl/piomin/samples/quarkus/serverless/order/service/OrderPublisher.java
@@ -1,50 +1,38 @@
 package pl.piomin.samples.quarkus.serverless.order.service;
 
-import io.smallrye.mutiny.Multi;
-import io.smallrye.reactive.messaging.annotations.Broadcast;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
+import pl.piomin.samples.quarkus.serverless.order.client.OrderSender;
 import pl.piomin.samples.quarkus.serverless.order.model.Order;
 import pl.piomin.samples.quarkus.serverless.order.model.OrderStatus;
 import pl.piomin.samples.quarkus.serverless.order.repository.OrderRepository;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.transaction.UserTransaction;
-import java.time.Duration;
+import javax.transaction.Transactional;
 
 @ApplicationScoped
 public class OrderPublisher {
 
     private static long num = 0;
 
-    @Inject
-    Logger log;
-    @Inject
-    OrderRepository repository;
-    @Inject
-    UserTransaction transaction;
-    @ConfigProperty(name = "app.orders.timeout", defaultValue = "10000")
-    int timeout;
+    private final Logger log;
+    private final OrderRepository repository;
+    private final OrderSender sender;
 
-    @Outgoing("order-events")
-    @Broadcast
-    public Multi<Order> orderEventsPublish() {
-        return Multi.createFrom().ticks().every(Duration.ofMillis(timeout))
-                .map(tick -> {
-                    Order o = new Order(null, (int) ++num%1000+1, (int) num%100+1, 100, 1, OrderStatus.NEW);
-                    try {
-                        transaction.begin();
-                        repository.persist(o);
-                        transaction.commit();
-                    } catch (Exception e) {
-                        log.error("Error in transaction", e);
-                    }
+    public OrderPublisher(Logger log,
+                          OrderRepository repository,
+                          @RestClient OrderSender sender) {
+        this.log = log;
+        this.repository = repository;
+        this.sender = sender;
+    }
 
-                    log.infof("Order published: %s", o);
-                    return o;
-                });
+    @Transactional
+    public void generate() {
+        Order o = new Order(null, (int) ++num%1000+1, (int) num%100+1, 100, 1, OrderStatus.NEW);
+        repository.persist(o);
+        sender.send(o);
+        log.infof("Order published: %s", o);
     }
 
 }
