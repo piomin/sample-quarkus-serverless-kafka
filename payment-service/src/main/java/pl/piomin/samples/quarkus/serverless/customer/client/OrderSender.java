98b09f4238fabfd305d507dca78e7ac22ea311e9
piomin <piotr.minkowski@gmail.com>
2023-04-17 09:47:47 +0200

    demo

--- payment-service/src/main/java/pl/piomin/samples/quarkus/serverless/customer/client/OrderSender.java
+++ payment-service/src/main/java/pl/piomin/samples/quarkus/serverless/customer/client/OrderSender.java
@@ -1,20 +1,12 @@
 package pl.piomin.samples.quarkus.serverless.customer.client;
 
-import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
-import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import pl.piomin.samples.quarkus.serverless.customer.message.Order;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
 
 @ApplicationScoped
-@RegisterRestClient
-@RegisterClientHeaders(CloudEventHeadersFactory.class)
 public interface OrderSender {
 
-    @POST
-    @Path("/payment-sink")
     void send(Order order);
 
 }
--- payment-service/src/main/java/pl/piomin/samples/quarkus/serverless/customer/function/OrderReserveFunction.java
+++ payment-service/src/main/java/pl/piomin/samples/quarkus/serverless/customer/function/OrderReserveFunction.java
@@ -1,10 +1,7 @@
 package pl.piomin.samples.quarkus.serverless.customer.function;
 
-import io.quarkus.funqy.Funq;
 import org.jboss.logging.Logger;
 import pl.piomin.samples.quarkus.serverless.customer.message.Order;
-import pl.piomin.samples.quarkus.serverless.customer.message.OrderStatus;
-import pl.piomin.samples.quarkus.serverless.customer.model.Customer;
 import pl.piomin.samples.quarkus.serverless.customer.service.OrderConfirmService;
 import pl.piomin.samples.quarkus.serverless.customer.service.OrderReserveService;
 
@@ -24,14 +21,8 @@ public class OrderReserveFunction {
         this.orderConfirmService = orderConfirmService;
     }
 
-    @Funq
     public void reserve(Order order) {
         log.infof("Received order: %s", order);
-        if (order.getStatus() == OrderStatus.NEW) {
-            orderReserveService.doReserve(order);
-        } else {
-            orderConfirmService.doConfirm(order);
-        }
     }
 
 }
